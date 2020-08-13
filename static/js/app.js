60cf8424e2b7576f129f984077fcea1da6aec439
Nethra3698 <nethra.rajavel@gmail.com>
2020-08-12 21:25:38 -0400

    Challenge

--- static/js/app.js
+++ static/js/app.js
@@ -15,13 +15,29 @@ function buildTable(data) {
         });
 	});
   };
-  function handleClick() {
+  function inputFilters() {
     let date = d3.select("#datetime").property("value");
+    let city = d3.select("#city").property("value");
+    let state = d3.select("#state").property("value");
+    let country = d3.select("#country").property("value");
+    let shape = d3.select("#shape").property("value");
     let filteredData = tableData;
     if (date) {
         filteredData = filteredData.filter(row => row.datetime === date);
     };
+    if (city) {
+      filteredData = filteredData.filter(row => row.city === city);
+    };
+    if (state) {
+    filteredData = filteredData.filter(row => row.state === state);
+    };
+    if (country) {
+      filteredData = filteredData.filter(row => row.country === country);
+    };
+    if (shape) {
+      filteredData = filteredData.filter(row => row.shape === shape);
+    };
     buildTable(filteredData);
-  };
-  d3.selectAll("#filter-btn").on("click", handleClick);
+  };  
+  d3.selectAll("#filter-btn").on("click", inputFilters);
   buildTable(tableData);
